f09ba3452d9d6511872f3af7cd266de26cba7559
tochilavictor <tochilavictor@gmail.com>
2017-07-12 16:44:47 +0300

    Interface realization

--- SortLogic.Test/JuggedArraySortTest.cs
+++ SortLogic.Test/JuggedArraySortTest.cs
@@ -10,7 +10,122 @@ namespace SortLogic.Test
 {
     [TestFixture()]
     public class JuggedArraySortTest
-    { 
-       
+    {
+        [TestCase(
+            new int[] { 10, 40, 20 },
+            new int[] { 70, 123, -150 },
+            new int[] { 0 },
+            new int[] { 50, 50 },ExpectedResult = true)]
+        public bool BubbleSort_SumAscendingCriterium_PositiveTest(params int[][] array)
+        {
+            JuggedArraySort.BubbleSort.Sort(array,new JuggedSum());
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                if (array[i].Sum() > array[i + 1].Sum()) return false;
+            }
+            return true;
+        }
+
+        [TestCase(
+            new int[] { 10, 40, 20 },
+            new int[] { 70, 123, -150 },
+            new int[] { 0 },
+            new int[] { 50, 50 }, ExpectedResult = true)]
+        public bool BubbleSort_SumDescendingCriterium_PositiveTest(params int[][] array)
+        {
+            JuggedArraySort.BubbleSort.Sort(array, new JuggedSum(),false);
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i].Sum() < array[i + 1].Sum()) return false;
+            }
+            return true;
+        }
+
+        [TestCase(
+            new int[] { 10, 40, 20 },
+            new int[] { 70, 123, -150 },
+            new int[] { 0 },
+            new int[] { 50, 50 }, ExpectedResult = true)]
+        public bool BubbleSort_MaxAscendingCriterium_PositiveTest(params int[][] array)
+        {
+            JuggedArraySort.BubbleSort.Sort(array, new JuggedMax());
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i].Max() > array[i + 1].Max()) return false;
+            }
+            return true;
+        }
+
+        [TestCase(
+            new int[] { 10, 40, 20 },
+            new int[] { 70, 123, -150 },
+            new int[] { 0 },
+            new int[] { 50, 50 }, ExpectedResult = true)]
+        public bool BubbleSort_MaxDescendingCriterium_PositiveTest(params int[][] array)
+        {
+            JuggedArraySort.BubbleSort.Sort(array, new JuggedMax(),false);
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i].Max() < array[i + 1].Max()) return false;
+            }
+            return true;
+        }
+
+        [TestCase(
+            new int[] { 10, 40, 20 },
+            new int[] { 70, 123, -150 },
+            new int[] { 0 },
+            new int[] { 50, 50 }, ExpectedResult = true)]
+        public bool BubbleSort_MinAscendingCriterium_PositiveTest(params int[][] array)
+        {
+            JuggedArraySort.BubbleSort.Sort(array, new JuggedMin());
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i].Min() > array[i + 1].Min()) return false;
+            }
+            return true;
+        }
+
+        [TestCase(
+            new int[] { 10, 40, 20 },
+            new int[] { 70, 123, -150 },
+            new int[] { 0 },
+            new int[] { 50, 50 }, ExpectedResult = true)]
+        public bool BubbleSort_MinDescendingCriterium_PositiveTest(params int[][] array)
+        {
+            JuggedArraySort.BubbleSort.Sort(array, new JuggedMin(), false);
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i].Min() < array[i + 1].Min()) return false;
+            }
+            return true;
+        }
+    }
+    public class JuggedSum : JuggedArraySort.IComparer
+    {
+        public int CompareTo(int[] lhs, int[] rhs)
+        {
+            if (lhs.Sum() == rhs.Sum()) return 0;
+            if (lhs.Sum() > rhs.Sum()) return 1;
+            return -1;
+        }
+    }
+    public class JuggedMax : JuggedArraySort.IComparer
+    {
+        public int CompareTo(int[] lhs, int[] rhs)
+        {
+            if (lhs.Max() == rhs.Max()) return 0;
+            if (lhs.Max() > rhs.Max()) return 1;
+            return -1;
+        }
+    }
+    public class JuggedMin : JuggedArraySort.IComparer
+    {
+        public int CompareTo(int[] lhs, int[] rhs)
+        {
+            if (lhs.Min() == rhs.Min()) return 0;
+            if (lhs.Min() > rhs.Min()) return 1;
+            return -1;
+        }
     }
 }
--- SortLogic/JuggedArraySort.cs
+++ SortLogic/JuggedArraySort.cs
@@ -5,91 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SortLogic
-{
-    public enum SortingOrder { Growing, Waning }
-    public enum SortingСriterion { Sum, Max, Min }
-   
+{  
     public static class JuggedArraySort
     {
-        /// <summary>
-        /// Sorts strings of matrix in order specified by criterion and order
-        /// </summary>
-        /// <param name="array">jugged integer array</param>
-        /// <param name="sortingCriterion">member of enum SortingСriterion</param>
-        /// <param name="sortingOrder">member of enum SortingСriterion(growing,waning)</param>
-        /// <exception cref="ArgumentException">invalid member of enum is used</exception>
-        public static void BubbleSortMatrix(int[][] array, SortingСriterion sortingCriterion, SortingOrder sortingOrder)
+        public interface IComparer
         {
-            ValidateJuggedArray(array);
-
-            int numberOfStrings = array.Length;
-            int[] criterionArray = new int[numberOfStrings];
-
-            switch (sortingCriterion)
-            {
-                case SortingСriterion.Sum:
-                    for (int i = 0; i < criterionArray.Length; i++)
-                    {
-                        for (int j = 0; j < array[i].Length; j++)
-                            criterionArray[i] += array[i][j];
-                    }
-                    break;
-                case SortingСriterion.Max:
-                    for (int i = 0; i < criterionArray.Length; i++)
-                    {
-                        criterionArray[i] = array[i][0];
-                        for (int j = 0; j < array[i].Length; j++)
-                            if (criterionArray[i] < array[i][j]) criterionArray[i] = array[i][j];
-                    }
-                    break;
-                case SortingСriterion.Min:
-                    for (int i = 0; i < criterionArray.Length; i++)
-                    {
-                        criterionArray[i] = array[i][0];
-                        for (int j = 0; j < array[i].Length; j++)
-                            if (criterionArray[i] > array[i][j]) criterionArray[i] = array[i][j];
-                    }
-                    break;
-                default: throw new ArgumentException($"Invalid {nameof(sortingCriterion)}");
-            }
-
-            if (sortingOrder == SortingOrder.Growing)
-            {
-                for (int i = 0; i < criterionArray.Length - 1; i++)
-                {
-                    for (int j = i + 1; j < criterionArray.Length; j++)
-                    {
-                        if (criterionArray[i] > criterionArray[j])
-                        {
-                            Swap(ref criterionArray[i], ref criterionArray[j]);
-                            Swap(ref array[i], ref array[j]);
-                        }
-                    }
-                }
-            }
-
-            else if (sortingOrder == SortingOrder.Waning)
-            {
-                for (int i = 0; i < criterionArray.Length - 1; i++)
-                {
-                    for (int j = i + 1; j < criterionArray.Length; j++)
-                    {
-                        if (criterionArray[i] < criterionArray[j])
-                        {
-                            Swap(ref criterionArray[i], ref criterionArray[j]);
-                            Swap(ref array[i], ref array[j]);
-                        }
-                    }
-                }
-            }
-
-            else throw new ArgumentException($"Invalid {nameof(sortingOrder)}");
+            int CompareTo(int[] lhs, int[] rhs);
         }
-        private static void Swap(ref int l, ref int r)
+        public static class BubbleSort
         {
-            int tmp = l;
-            l = r;
-            r = tmp;
+            public static void Sort(int[][] array, IComparer ic, bool ifAscendingOrder = true)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (ifAscendingOrder ? ic.CompareTo(array[i], array[j]) == 1 : ic.CompareTo(array[i], array[j]) == -1)
+                        {
+                            var tmp = array[i];
+                            array[i] = array[j];
+                            array[j] = tmp;
+                        }
+                    }
+                }
+            }
         }
         private static void Swap(ref int[] l, ref int[] r)
         {
